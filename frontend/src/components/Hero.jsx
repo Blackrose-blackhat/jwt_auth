dbb419fa2e4c38a0f6c9140ebcfba78030bb390a
musharaf8651 <musharafz2k3@gmail.com>
2023-07-21 12:47:08 +0530

    frontend started

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap';
+import { Card, Container, Button } from 'react-bootstrap';
+const Hero = () => {
+    return (
+        <div className="py-5">
+            <Container className='d-flex justify-content-center'>
+                <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
+                    <h1 className="text-center mb-4">
+                        MERN Authentication
+                    </h1>
+                    <p className="text-center mb-4">
+                        This is a simple authentication system built using MongoDB Atlas for database and Express.js with JWT tokens
+                    </p>
+                    <div className="d-flex">
+                        <LinkContainer to='/login'>
+                            <Button variant='primary' className='me-3'>
+                                Sign In
+                            </Button>
+                        </LinkContainer>
+
+                        <LinkContainer to='/register'>
+                            <Button variant='secondary' >
+                                Register
+                            </Button>
+                        </LinkContainer>
+                    </div>
+                </Card>
+            </Container>
+        </div>
+    );
+}
+
+export default Hero
